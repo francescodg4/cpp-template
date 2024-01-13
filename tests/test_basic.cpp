7ed0e4a02a8c5885948eef003bd0c563ae8f803a
Francesco de Gioia <francescodg4@gmail.com>
2024-01-13 16:43:34 +0100

    updated catch with new fetch content integration

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -1,20 +1,35 @@
-#include "catch.hpp"
+#include "mylibrary.hpp"
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 static int Factorial(int number)
 {
-    // return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
-    return number <= 1 ? 1 : Factorial(number - 1) * number; // pass
+    return number <= 1 ? 1 : Factorial(number - 1) * number;
 }
 
-TEST_CASE("Factorial of 0 is 1 (fail)", "[single-file]")
-{
-    REQUIRE(Factorial(0) == 1);
-}
-
-TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]")
+TEST_CASE("Factorial tests", "[factorial]")
 {
     REQUIRE(Factorial(1) == 1);
     REQUIRE(Factorial(2) == 2);
     REQUIRE(Factorial(3) == 6);
     REQUIRE(Factorial(10) == 3628800);
+}
+
+TEST_CASE("Factorial benchmark", "[benchmark]")
+{
+    BENCHMARK("Factorial 25")
+    {
+        return Factorial(25);
+    };
+
+    BENCHMARK("Factorial 50")
+    {
+        return Factorial(50);
+    };
+}
+
+TEST_CASE("Check mylibrary::function", "[mylibrary]")
+{
+    REQUIRE(mylibrary::function(4, 5) == 9);
+    REQUIRE(mylibrary::function(3, 4) == 7);
 }
