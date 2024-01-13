ca788635be24caeedd5a0acb3ff35e3ae30da425
Francesco de Gioia <francescodg4@gmail.com>
2024-01-13 16:45:32 +0100

    updated sample code with integration of mylibrary function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
+#include "mylibrary.hpp"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, world\n";
+    std::cout << "Call function: " << mylibrary::function(1, 2) << '\n';
     return 0;
 }
