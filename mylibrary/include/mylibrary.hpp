40edbd0a1cfafd8c69dedb969510b7b69c31abc5
Francesco de Gioia <francescodg4@gmail.com>
2024-01-13 16:44:18 +0100

    added mylibrary subproject as static library

---
+++ mylibrary/include/mylibrary.hpp
@@ -0,0 +1,10 @@
+#ifndef MY_LIBRARY_HPP
+#define MY_LIBRARY_HPP
+
+namespace mylibrary {
+
+int function(int a, int b);
+
+} // namespace mylibrary
+
+#endif /* MY_LIBRARY_HPP */
---
+++ mylibrary/src/mylibrary.cpp
@@ -0,0 +1,10 @@
+#include "mylibrary.hpp"
+
+namespace mylibrary {
+
+int function(int a, int b)
+{
+    return a + b;
+}
+
+} // namespace mylibrary
