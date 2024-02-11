63496fc2b296171a2423a9061c3f84de46598dce
Francesco de Gioia <francescodg4@gmail.com>
2024-02-11 12:04:17 +0100

    explicit function export for dynamic libraries

--- mylibrary/include/mylibrary.hpp
+++ mylibrary/include/mylibrary.hpp
@@ -1,14 +1,28 @@
 #ifndef MY_LIBRARY_HPP
 #define MY_LIBRARY_HPP
 
-extern "C" void foo();
+#if defined _WIN32 || defined __CYGWIN__
+#define SYMBOL_EXPORT __declspec(dllexport)
+#define SYMBOL_IMPORT __declspec(dllimport)
+#else
+#define SYMBOL_EXPORT __attribute__((visibility("default")))
+#define SYMBOL_IMPORT
+#endif
+
+#ifdef DLL_EXPORTS
+#define MY_LIBRARY_API SYMBOL_EXPORT
+#else
+#define MY_LIBRARY_API SYMBOL_IMPORT
+#endif
+
+extern "C" MY_LIBRARY_API void foo();
 
 namespace mylibrary {
 
 /**
  * @brief Returns the sum of two numbers
  */
-int sum(int a, int b);
+MY_LIBRARY_API int sum(int a, int b);
 
 } // namespace mylibrary
 
