0caa6d16550ab706ced4afd8125229cdacbead37
Francesco de Gioia <francescodg4@gmail.com>
2024-03-03 21:56:25 +0100

    build: added example of conanfile.py

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include "mylibrary.hpp"
 #include <iostream>
+#include <spdlog/spdlog.h>
 
 int main()
 {
     std::cout << "Call function: " << mylibrary::sum(1, 2) << '\n';
     mylibrary::examples::std_vector_custom_allocator();
+
+#ifndef NDEBUG
+    spdlog::set_level(spdlog::level::debug);
+#endif
+
+    spdlog::debug("Debug - Hello, world!");
+    spdlog::info("Hello, world!");
+
     return 0;
 }
