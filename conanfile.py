652333e23886d7e6f11ade95f1b91f56d88942b4
Francesco de Gioia <francescodg4@gmail.com>
2024-03-04 08:53:24 +0100

    build: added dependency to sdl2 with simple example

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,8 @@ class ExampleRecipe(ConanFile):
 
     def requirements(self):
         self.requires("spdlog/1.13.0")
+        self.requires("sdl/2.28.3")
+        self.requires("sdl_image/2.6.3")
 
     def layout(self):
         cmake_layout(self)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include "mylibrary.hpp"
+#include <SDL2/SDL.h>
 #include <iostream>
 #include <spdlog/spdlog.h>
 
 int main()
 {
+    const char* WindowName = "Test SDL Application";
+    const unsigned int ScreenWidth = 800;
+    const unsigned int ScreenHeight = 600;
+
     std::cout << "Call function: " << mylibrary::sum(1, 2) << '\n';
     mylibrary::examples::std_vector_custom_allocator();
 
@@ -13,6 +18,49 @@ int main()
 
     spdlog::debug("Debug - Hello, world!");
     spdlog::info("Hello, world!");
+
+    bool done = false;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    auto window = SDL_CreateWindow(
+        WindowName,
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        ScreenWidth,
+        ScreenHeight,
+        SDL_WINDOW_SHOWN);
+
+    auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 255);
+
+    while (!done) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                done = true;
+                break;
+            default:
+                break;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+
+        spdlog::info("loop");
+
+        SDL_Delay(15);
+    }
+
+    spdlog::info("cleanup");
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+
+    SDL_Quit();
 
     return 0;
 }
